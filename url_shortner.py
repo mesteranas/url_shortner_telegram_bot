aae76610ecb4747de9f5673a5cb7448767fdc2a0
mesteranas <135442710+mesteranas@users.noreply.github.com>
2023-11-29 13:35:06 +0200

    29/11/2023-c=2

--- url_shortner.py
+++ url_shortner.py
@@ -15,12 +15,16 @@ def short(UR):
 
 def message(msg):
     content_type,chat_type,chat_id=telepot.glance(msg)
-    if msg["text"]=="/start":
-        bot.sendMessage(chat_id,"welcome to this bot , this bot make you to short the link ,please send me the link and i short it")
+    if content_type == 'text':
+        if msg["text"]=="/start":
+           bot.sendMessage(chat_id, f"Welcome {msg['from']['first_name']} to this bot. This bot allows you to shorten links. Please send me the link, and I'll shorten it.")
+        else:
+            bot.sendMessage(chat_id,"result={}".format(short(msg["text"])))
     else:
-        bot.sendMessage(chat_id,"result={}".format(short(msg["text"])))
+        bot.sendMessage(chat_id,"please send text messages only")
 
-bot=telepot.Bot("6940359512:AAEmKJi2G8K7L3cs0ud7g4vjMxq87tvLarU")
+bot=telepot.Bot("id")
+bot.deleteWebhook()
 MessageLoop(bot,{"chat":message}).run_as_thread()
 print("runing")
 while True:
