de1bc86df3b436b1cd5ab64192967e90b2d4426b
mesteranas <135442710+mesteranas@users.noreply.github.com>
2023-12-11 11:54:55 +0200

    11/12/2023

--- url_shortner.py
+++ url_shortner.py
@@ -1,6 +1,7 @@
 import requests
 import urllib
 import telepot
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from telepot.loop import MessageLoop
 def short(UR):
     link = "http://tinyurl.com/api-create.php"
@@ -19,11 +20,16 @@ def message(msg):
         if msg["text"]=="/start":
            bot.sendMessage(chat_id, f"Welcome {msg['from']['first_name']} to this bot. This bot allows you to shorten links. Please send me the link, and I'll shorten it.")
         else:
-            bot.sendMessage(chat_id,"result={}".format(short(msg["text"])))
+            re=short(msg["text"])
+            if not re=="Error":
+                keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Open Link', url=re)]])
+                bot.sendMessage(chat_id,"result={}".format(re),reply_markup=keyboard)
+            else:
+                bot.sendMessage(chat_id,"result={}".format(re))
     else:
         bot.sendMessage(chat_id,"please send text messages only")
 
-bot=telepot.Bot("id")
+bot=telepot.Bot("token")
 bot.deleteWebhook()
 MessageLoop(bot,{"chat":message}).run_as_thread()
 print("runing")
